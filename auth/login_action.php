dd82584a33de8a66564bf4113081bcffa834707a
Geleta Tamiru <geletatamiru@yahoo.com>
2025-05-25 16:57:55 +0300

    reduced the sizes of the image.

--- auth/login_action.php
+++ auth/login_action.php
@@ -18,9 +18,9 @@ if (mysqli_num_rows($result) === 1) {
     if (password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['name'] = $user['name'];
-        $_SESSION['is_admin'] = $user['is_admin'];
+        $_SESSION['is_admin'] = $user['role'];
 
-        if ($user['is_admin']) {
+        if ($user['is_admin'] === "admin") {
             header("Location: ../admin/admin_dashboard.php");
         } else {
             header("Location: ../index.php");
